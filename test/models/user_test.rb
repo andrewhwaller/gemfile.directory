0a5367a13eae7eced8525aae76aac175faf2e58c
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-02-27 17:49:29 -0600

    Add basic validation tests for users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,65 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user_one = users(:user_one)
+    @user_two = users(:user_two)
+  end
+
+  test "provider cannot be nil" do
+    user = User.new(
+      uid: "123",
+      email: "test@example.com"
+    )
+    user.valid?
+    assert user.errors.of_kind? :provider, :blank
+  end
+
+  test "uid cannot be nil" do
+    user = User.new(
+      provider: "github",
+      email: "test@example.com"
+    )
+    user.valid?
+    assert user.errors.of_kind? :uid, :blank
+  end
+
+  test "uid must be unique" do
+    user = User.new(
+      provider: "github",
+      uid: @user_one.uid,
+      email: "test@example.com"
+    )  
+    user.valid?
+    assert user.errors.of_kind? :uid, :taken
+  end
+
+  test "email must be a valid email" do
+    user = User.new(
+      provider: "github",
+      uid: "123",
+      email: "test"
+    )
+    user.valid?
+    assert user.errors.of_kind? :email, :invalid
+  end
+
+  test "email cannot be nil" do
+    user = User.new(
+      provider: "github",
+      uid: "123"
+    )
+    user.valid?
+    assert user.errors.of_kind? :email, :blank
+  end
+
+  test "email must be unique" do
+    user = User.new(
+      provider: "github",
+      uid: "123",
+      email: @user_one.email
+    )
+    user.valid?
+    assert user.errors.of_kind? :email, :taken
+  end
 end
