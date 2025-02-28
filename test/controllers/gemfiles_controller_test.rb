2d4df65ea5eb500c3ce4736815d7ecdf6a2f4596
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-02-27 19:58:32 -0600

    Remove pending tests

--- test/controllers/gemfiles_controller_test.rb
+++ test/controllers/gemfiles_controller_test.rb
@@ -18,45 +18,4 @@ class GemfilesControllerTest < ActionDispatch::IntegrationTest
     get new_gemfile_url
     assert_response :success
   end
-
-  test "should create gemfile" do
-    skip "pending"
-
-    assert_difference("Gemfile.count") do
-      post gemfiles_url, params: { gemfile: { content: "gem 'rails'", app_link: "https://example.org", user_id: @gemfile.user_id } }
-    end
-
-    assert_redirected_to gemfile_url(Gemfile.order(created_at: :asc).last)
-  end
-
-  test "should show gemfile" do
-    skip "pending"
-
-    get gemfile_url(@gemfile)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    skip "pending"
-
-    get edit_gemfile_url(@gemfile)
-    assert_response :success
-  end
-
-  test "should update gemfile" do
-    skip "pending"
-
-    patch gemfile_url(@gemfile), params: { gemfile: { content: @gemfile.content, user_id: @gemfile.user_id } }
-    assert_redirected_to gemfile_url(@gemfile)
-  end
-
-  test "should destroy gemfile" do
-    skip "pending"
-
-    assert_difference("Gemfile.count", -1) do
-      delete gemfile_url(@gemfile)
-    end
-
-    assert_redirected_to gemfiles_url
-  end
 end
