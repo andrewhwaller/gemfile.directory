273f136569f640c2edbb46ef77f28ed14a1d73fd
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-02-27 21:37:24 -0600

    Add authenticate method so that specific users can be authed in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,6 +17,31 @@ module ActiveSupport
         OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(Faker::Omniauth.github)
         Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:github]
       end
+
+      def authenticate(user:)
+        # Set up OmniAuth test mode
+        OmniAuth.config.test_mode = true
+        
+        # Create auth hash that matches the user's data
+        auth_hash = OmniAuth::AuthHash.new(
+          provider: user.provider,
+          uid: user.uid,
+          info: {
+            email: user.email,
+            name: user.name,
+            image: user.image,
+            nickname: user.github_username
+          }
+        )
+        
+        # Set up the auth hash in the test environment
+        OmniAuth.config.mock_auth[:github] = auth_hash
+        Rails.application.env_config["omniauth.auth"] = auth_hash
+        
+        # Simulate the OAuth callback
+        get "/auth/github/callback"
+        follow_redirect!
+      end
     end
   end
 end
