690b8c444f389c1859b5c49eabe26031a6098386
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-02-28 09:12:38 -0600

    you know what, nevermind

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,51 +1,6 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
-require "mock_redis"
-require "sidekiq/testing"
-
-Sidekiq::Testing.fake!
-
-class MockRedis
-  alias_method :original_hmset, :hmset
-  alias_method :original_setex, :setex
-  alias_method :original_mset, :mset
-
-  def hmset(key, *attrs)
-    normalized_attrs = attrs.each_slice(2).map { |field, value| [ field, normalize_value(value) ] }.flatten
-    original_hmset(key, *normalized_attrs)
-  end
-
-  def setex(key, ttl, value)
-    value = normalize_value(value)
-    original_setex(key, ttl, value)
-  end
-
-  def mset(*attrs)
-    normalized_attrs = attrs.each_slice(2).map { |key, value| [ key, normalize_value(value) ] }.flatten
-    original_mset(*normalized_attrs)
-  end
-
-  private
-
-  def normalize_value(value)
-    if value.is_a?(Hash) && value.default_proc
-      value = value.to_h { |k, v| [ k, v ] } # Convert to a normal hash
-    end
-    value
-  end
-end
-
-# Create a global MockRedis instance for consistency
-MOCK_REDIS = MockRedis.new
-
-Sidekiq.configure_server do |config|
-  config.redis = { namespace: "sidekiq_test", url: MOCK_REDIS }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { namespace: "sidekiq_test", url: MOCK_REDIS }
-end
 
 module ActiveSupport
   class TestCase
@@ -54,11 +9,6 @@ module ActiveSupport
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
-
-    setup do
-      @redis = MOCK_REDIS
-      @redis.flushdb # Ensure Redis is empty at the start of each test
-    end
 
     # Add more helper methods to be used by all tests here...
     module OmniauthGithubHelper
